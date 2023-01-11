d3e58b33fe0dacc0888b62104c11681045abb21e
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-11 22:39:42 +0100

    Done Task

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
 import "./App.css";
 import { Data } from "./Data";
 import { useState } from "react";
+import cart from "./cart.jpg";
 
 function App() {
   const [selected, setSelected] = useState({ show: false, item: {} });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {Data.products.map((item, index) => {
-          return (
+    <div id="main">
+      {Data.products.map((item, index) => {
+        return (
+          <div
+            className="divStyle"
+            onClick={() => setSelected({ show: true, item: item })}
+          >
             <div
-              style={{
-                width: 200,
-                height: 100,
-                float: "left",
-                border: "1px solid red",
-                padding: 5,
-              }}
-              onClick={() => setSelected({ show: true, item: item })}
-            >
-              {item.title}
+              id="thumbNail"
+              style={{ backgroundImage: `url(${item.thumbnail})` }}
+            ></div>
+            <div id="product">
+              <div id="title">
+                <p>{item.title}</p>
+              </div>
+              <div id="price">
+                <p>${item.price}</p>
+              </div>
+              <div
+                id="cart"
+                style={{
+                  backgroundImage: `url(${cart})`,
+                }}
+              ></div>
             </div>
-          );
-        })}
-      </header>
+            <div id="reszletek">
+              <button id="buttonDetails">Details</button>
+            </div>
+          </div>
+        );
+      })}
       {selected.show && (
-        <div
-          onClick={() => setSelected({ show: false, item: {} })}
-          style={{
-            position: "fixed",
-            width: 300,
-            height: 200,
-            top: 100,
-            left: 100,
-            border: "1px solid red",
-            background: "#fff",
-          }}
-        >
-          <li>{selected.item.title}</li>
-          <li>{selected.item.description}</li>
-          <li>
-            <img src={selected.item.thumbnail} alt="" width="90%" />
-          </li>
+        <div onClick={() => setSelected({ show: false, item: {} })} id="window">
+          <h2 className="dt">Details</h2>
+          <p>
+            <span>Title:</span> {selected.item.title}
+          </p>
+          <p>
+            <span>Description:</span> {selected.item.description}
+          </p>
+          <p>
+            <span>Discount:</span> ${selected.item.discountPercentage}
+          </p>
+          <p>
+            <span>Category:</span> {selected.item.category}
+          </p>
+          <div className="close">Close</div>
         </div>
       )}
     </div>
